e124e31ec46e81d678f1d0d3eb6ac15480fa909f
dboren <danielboren@att.net>
2021-05-18 21:20:48 -0500

    added html elements to Header component

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header class="container bg-danger border border-warning">
+        <div class="d-flex justify-content-between">
+            <div class="p-2">
+                <h1 class="text-white">My Portfolio</h1>
+            </div>
+            <div class="p-2"></div>
+            <div class="p-2 pull-right">
+                <nav>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item">
+                            <a href="index.html">About</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="portfolio.html">Portfolio</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>  
+                 </nav>
+            </div>
+        </div>
+    </header>
+    );
+};
+
+export default Header;
