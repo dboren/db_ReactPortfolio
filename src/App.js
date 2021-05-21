7aad3223a7def21b2e66a957df5792e031e952fa
dboren <danielboren@att.net>
2021-05-20 19:55:25 -0500

    added Router and Home to return block of App.js. Code is compiling but elements are still not rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 
 function App() {
-  
+  return (
+      <Router>
+          <div>
+              <Switch>
+                <Route exact path ="/" component={Home} />
+              </Switch>
+          </div>
+      </Router>
+  )
 }
 
 export default App;
