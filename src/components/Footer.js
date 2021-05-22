c2ea9ee982f96aa0a72b828a67d17049ac5d0c6c
dboren <danielboren@att.net>
2021-05-21 22:00:10 -0500

    added html to Footer element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-    return ()
+    return (
+        <footer class="container bg-danger border border-warning">
+        <p class="text-white">Copyright © Daniel Boren 2021</p>
+        </footer>
+    )
 }
 
 export default Footer;
