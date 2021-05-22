27a64fc7c2f039077fbcb6d8243da573e356c301
dboren <danielboren@att.net>
2021-05-21 22:03:39 -0500

    added Footer to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Bio from "../components/Bio";
+import Footer from "../components/Footer"
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Bio></Bio>
+            <Footer></Footer>
         </div>
     );
 };
