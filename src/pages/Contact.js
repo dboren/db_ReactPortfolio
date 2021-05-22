e7b7c952b0b6f5a9b25428d1ea15da5fb2bd2846
dboren <danielboren@att.net>
2021-05-21 22:46:46 -0500

    added ContactInfo component and imported it to Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ContactInfo from "../components/ContactInfo";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            
+            <ContactInfo></ContactInfo>
             <Footer></Footer>
         </div>
     );
