e0749e5f9fcb312905310c8c387cf6a286ce11d9
dboren <danielboren@att.net>
2021-05-21 22:30:24 -0500

    added test html to Project component and imported it to the Portfolio page to determine it is rendering. Still need to refactor to use props

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+const Project = (props) => {
+    return (
 
+        <content class="container">
+        <div class="row mt-3 border">
+            <div class="col">
+                <img src="https://github.com/dboren/db_UpdatedPortfolio2/blob/main/Assets/Images/Curatio_Screencap.JPG?raw=true" class="img-fluid" alt="Screenshot of Non-Profit Locator"></img>
+            </div>
+            <div class="col">
+                <h2>Curatio</h2>
+                <p>This application utlizes the Youtube API and MapBox API to allow users to put together curated youtube video playlists for hobbies of their choice, after which they can continue to add personal notes to specific videos they have saved. This is a true full stack application which saves user accounts along with their hobbies, videos, and notes in a database</p>
+                
+                <p>Created in collaboration with Mark Lloyd, Kayleigh Hanna, and Kanna Vairavan</p>
+                
+                <a href="https://github.com/m-llo/Project-2-Team-6">Repository</a>
+                
+                <a href="https://stormy-river-69253.herokuapp.com">Depoloyed page</a>
+            </div>
+        </div>
+    </content>
+
+    );
+};
 
 export default Project;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Project from "../components/Project";
 
 const Portfolio = () => {
     return (
         <div>
            <Header></Header>
-
+            <Project></Project>
            <Footer></Footer>
         </div>
     );
