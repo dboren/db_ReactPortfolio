5b4ab7ed9bd8b2e11b7b00f64de43548379dde84
dboren <danielboren@att.net>
2021-05-22 23:19:53 -0500

    corrected the link for the resume on the contact page

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -7,7 +7,7 @@ const ContactInfo = () => {
             <p class="offset-1"><strong>E-mail: </strong>danielboren52@gmail.com</p>
             <p class="offset-1"><strong>LinkedIn: </strong><a href="https://www.linkedin.com/in/daniel-boren-5b2b9864/">https://www.linkedin.com/in/daniel-boren-5b2b9864/</a></p>
             <p class="offset-1"><strong>GitHub: </strong><a href="https://github.com/dboren">https://github.com/dboren</a></p>
-            <p class="offset-1"><strong>Resume: </strong><a href="Assets/db_resume.pdf">Click here</a></p>
+            <p class="offset-1"><strong>Resume: </strong><a href="https://github.com/dboren/db_ReactPortfolio/raw/main/src/assets/db_resume.pdf">Click here</a></p>
         </div>
     )
 }
